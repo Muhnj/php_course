98d5a251f37bb4361f569c1feeb18a58d28a6e04
Munir <munirumujuzi@gmail.com>
2026-06-30 12:42:17 +0300

    commenting in php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-	$text = 'Hello World.';
+
+/*
+Author :Munir Mujuzi
+Date: 30/Jun/2026
+
+*/
+	$text = 'Hello World.';  //comments are very useful in php
+
+
 ?>
 
 <input type= "text" value="<?php echo $text; ?>">
