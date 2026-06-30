42e07d7b19df2c935e104287ddf2b73c13d42c2a
Munir <munirumujuzi@gmail.com>
2026-06-30 13:59:14 +0300

    if else statement

--- if_statements/index.php
+++ if_statements/index.php
@@ -1,10 +1,22 @@
 <?php
-$text = 'something';
+/*$text = 'something';
 	if($text == "somethings" ){
 		echo 'TRUE.';
 	}
 	else{
 		echo 'FALSE';
 	}
+
+	*/
+
+
+$number = 12;
+if ($number ==10) {
+	echo 'Equal to ten.';
+} else if ($number ==11){
+	echo 'Equal to Eleven';
+} else {
+	echo 'Not Equal.';
+}
 
 ?>
