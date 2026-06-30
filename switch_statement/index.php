f0903e60f6c7c142551a6c2d350009ee422290c3
Munir <munirumujuzi@gmail.com>
2026-06-30 15:58:00 +0300

    switch statement

---
+++ switch_statement/index.php
@@ -0,0 +1,35 @@
+<?php
+/*
+$number = 2;
+
+switch ($number) {
+	case 1:
+		echo 'One';
+	break;
+
+	case 2:
+		echo 'Two';
+	break;
+	case3:
+		echo 'Three';
+	break;
+
+	default:
+		echo 'Number not found.';
+	break;
+}
+*/
+
+
+$day = 'Monday';
+
+switch ($day){
+	case 'Saturday':
+	case 'Sunday':
+	echo 'It\'s a weekend';
+
+	default:
+		echo 'Not a weekend.';
+	break;
+}
+?>
