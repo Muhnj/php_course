006c8dbb3fffe218b28557a5ab0d375cfe252c2b
Munir <munirumujuzi@gmail.com>
2026-06-30 15:02:50 +0300

    Tripple equals operator

--- operators/index.php
+++ operators/index.php
@@ -8,9 +8,58 @@ $number2 /=2;
 echo $number2;
 */
 
+
+
+/*
 $text = 'Hello';
 $text .= 'World';
 
 echo $text;
+*/
+
+
+/*
+$number1 = 10;
+$number2 = 9;
+
+
+if ($number1 >= $number2){
+	echo 'yes.';
+} else {
+	echo 'No.';
+}
+
+
+$password = 'password';
+if ($password =='something'){
+	echo 'Correct!';
+} else{
+	echo 'Incorrect!';
+}
+*/
+
+/*
+$number1 = 100;
+$number2 = 50;
+$number3 = 2;
+
+
+$result = $number1 + $number2/ $number3;
+echo $result;   
+*/
+
+/*
+$number1 = 10;
+$number1 ++;
+echo $number1;
+*/
+
+$num1 = '1';
+$num2 = 1;
+if ($num1 === $num2){
+	echo 'Equal.';
+}else {
+	echo 'Not equal';
+}
 
 ?>
