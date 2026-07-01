306f9bb935fbf037fa755841abd5003b77f4f278
Munir <munirumujuzi@gmail.com>
2026-07-01 10:34:46 +0300

    Global variables and Functions

--- functions/index.php
+++ functions/index.php
@@ -24,12 +24,58 @@ function add ($number1, $number2){
 
 add ($inumber1, $inumber2);
 
-*/
+
+
+Example 3
 
 
 function displayDate($day, $date, $year) {
 	echo $day.' '.$date.' '.$year;
 }
  displayDate ('Monday', 31, 2011);
+
+
+ //function
+
+
+ function add ($number1, $number2){
+ 	$result = $number1 + $number2;
+ 	return $result;
+
+ }
+
+ (10)
+
+//function with a return value ,
+
+function add ($number1, $number2){
+	$result = $number1 + $number2;
+	return $result;
+
+}
+function divide ($number1, $number2){
+	$result = $number1/ $number2;
+	return $result;
+}
+
+$sum = divide (add(10,10), add (5, 5));
+echo $sum;
+
+*/
+
+
+//Global variables and Functions
+
+$user_ip = $_SERVER['REMOTE_ADDR'];
+
+
+function echo_ip() {
+	global $user_ip;
+	$string = 'Your IP address is' .$user_ip;
+	echo  $string;
+}
+
+echo_ip();
+
 
 ?>
