66dbd0f9f14538a32436ac0404a5ef5432b10e54
Munir <munirumujuzi@gmail.com>
2026-07-01 14:24:48 +0300

    For each and multidimensional Array

--- arrays/index.php
+++ arrays/index.php
@@ -1,5 +1,5 @@
 <?php
-/*//basic Arrays
+/*//basic Arrays and Associative Arrays
 $food = array('Pasta'=>300, 'Pizza'=>1000, 'Salad'=>150);
 $names = ['Jed ', 'Wahab', 'Ivan', 'Victor','Munir'];
 echo $names[2],
@@ -7,15 +7,11 @@ $food[1];
 $food[4] = 'Vegetable';
 
 print_r($food);
-
-//Associative Arrays
-
 echo $food['Pizza'];
 
 
 
 // multi dimension Arrays
-*/
 
 $food = array('Healthy'=>
 						array('Salad', 'vegartables', 'Pasta'),
@@ -23,5 +19,27 @@ $food = array('Healthy'=>
 				array ('Pizza', 'Ice cream'));
 
 echo $food['Unhealthy'] [1];
+
+
+*/
+
+
+// For Each 
+$food = array('Healthy'=>
+						array('Salad', 'vegartables', 'Pasta'),
+				'Unhealthy'=> 
+				array ('Pizza', 'Ice cream', 'Pop corn'));
+
+
+foreach ($food as $element => $inner_array) {
+	echo '<strong>'.$element.'</strong><br>';
+	echo $inner_element. '<br>';
+	foreach ($inner_array as $item) {
+		echo $item .'<br>';
+
+	}
+}
+
+
 
 ?>
