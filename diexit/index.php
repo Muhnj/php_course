0ceabd68e42d0cb7113b9d9bd81510ec87dc1aea
Munir <munirumujuzi@gmail.com>
2026-07-01 09:19:54 +0300

    die and exit function

--- diexit/index.php
+++ diexit/index.php
@@ -1,11 +1,16 @@
 <?php
  
-
+/*
  echo 'Hello';
 
  die ('ERROR. PAGE HAS ENDED.');
 
  echo 'World';
+*/
+
+
+@myysql_connect('localhost', 'root', '') || die ('Could not connect to database .');
+echo 'Connected !';
 
 ?>
 
