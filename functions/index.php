443f33a1a7db6a762c4cee160549b7f154187c59
Munir <munirumujuzi@gmail.com>
2026-07-01 09:53:04 +0300

    Functions

--- functions/index.php
+++ functions/index.php
@@ -1,4 +1,8 @@
 <?php
+/*
+
+functions
+
 
 function myName(){
 	echo 'Munir';
@@ -7,5 +11,25 @@ function myName(){
 
 echo 'My name is ';
 myName();
+
+
+example 2 
+
+$inumber1 = 10;
+$inumber2 = 5;
+
+function add ($number1, $number2){
+	echo $number1 + $number2;
+}
+
+add ($inumber1, $inumber2);
+
+*/
+
+
+function displayDate($day, $date, $year) {
+	echo $day.' '.$date.' '.$year;
+}
+ displayDate ('Monday', 31, 2011);
 
 ?>
