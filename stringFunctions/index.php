2f935b55fb90fcf74c8df6c867f012f4df2a8fad
Munir <munirumujuzi@gmail.com>
2026-07-01 10:54:17 +0300

    string shuffle

---
+++ stringFunctions/index.php
@@ -0,0 +1,18 @@
+<?php 
+/*
+//string functions 
+
+$string = 'This is an example string & this is a tutorial .';
+$string_word_count = str_word_count($string, 1, '&.');
+
+print_r($string_word_count);
+
+*/
+
+// string _shuffled .
+$string = 'This is an example string.';
+$string_shuffled = str_shuffle($string);
+
+echo $string_shuffled
+
+?>
