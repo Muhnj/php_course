88d53e4f3d0a8465fe270873769a392a6d760392
Munir <munirumujuzi@gmail.com>
2026-07-02 07:28:11 +0300

    word censoring

--- strings/index.php
+++ strings/index.php
@@ -41,7 +41,8 @@ if (isset($_GET['user_name']) &&!empty($_GET['user_name'])){
 
 
 
-*/
+
+// string Position function
 
 $offset = 0;
 
@@ -55,7 +56,38 @@ while ($string_position = strpos($string, $find, $offset)) {
 
 }
 
+
+//substring replace
+$string = 'This part don\'t search. This part search.';
+$string_new = substr_replace($string, 'Munir', 29, 4);
+
+echo $string_new;
+
+
+// example 2 
+$find = array('is', 'string', 'example');
+$replace = array('Is', 'STRING', '');
+$string = 'This is a string, and is an example.';
+$new_string = str_replace($find, $replace, $string);
+echo $new_string;
+
+
+*/
+if (isset($_POST['user_input']) && ! empty ($_POST['user_input'])) {
+	echo $user_input = $_POST['user_input'];
+
+}
+
 ?>
+<hr>
+<form action="index.php" method ="POST">
+<textarea name="user_input" rows="6" cols="30"><?php echo $user_input; ?></textarea><br><br>
+<input type="submit" value="submit">
+</form>
+
+
+
+
 
 
 
